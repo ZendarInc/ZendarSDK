4f615d07c7ce56499e177c32ae754319c2feeb0f
Gilbert Hsyu <ghsyu@zendar.io>
2021-02-08 19:30:09 -0800

    PR changes

--- cpp/example-receiver.cc
+++ cpp/example-receiver.cc
@@ -6,10 +6,20 @@ int main(int argc, char* argv[]) {
     printf("Usage: example-receiver URI");
   }
   zendar::ZendarReceiver rcv(argv[1]);
-  rcv.SubscribeImages(100);
-  rcv.SubscribeTracker(100);
-  rcv.SubscribeTracklog(100);
-  rcv.SubscribeLogs(100);
+  zendar::ZendarError error;
+
+  error = rcv.SubscribeImages(100);
+  if (error)
+    printf("Failed to subscribe to images");
+  error = rcv.SubscribeTracker(100);
+  if (error)
+    printf("Failed to subscribe to tracker");
+  error = rcv.SubscribeTracklog(100);
+  if (error)
+    printf("Failed to subscribe to tracklog");
+  error = rcv.SubscribeLogs(100);
+  if (error)
+    printf("Failed to subscribe to logs");
 
   printf("Successful Initialization\n");
   zen_proto::data::Image image;
@@ -17,8 +27,7 @@ int main(int argc, char* argv[]) {
   zen_proto::data::Position position;
   zen_proto::data::LogRecord log;
 
-  zendar::ZendarError error;
-  for (int i=0; i<10; ++i) {
+  for (int i = 0; i < 10; ++i) {
     error = rcv.NextImage(image);
     if (error) {
       printf("Failed to receive image\n");
@@ -48,9 +57,18 @@ int main(int argc, char* argv[]) {
       printf("Received log: %s\n", log.message().c_str());
     }
   }
-  rcv.UnsubscribeImages();
-  rcv.UnsubscribeTracker();
-  rcv.UnsubscribeTracklog();
-  rcv.UnsubscribeLogs();
+  error = rcv.UnsubscribeImages();
+  if (error)
+    printf("Failed to unsubscribe from images");
+  error = rcv.UnsubscribeTracker();
+  if (error)
+    printf("Failed to unsubscribe from tracker");
+  error = rcv.UnsubscribeTracklog();
+  if (error)
+    printf("Failed to subscribe from tracklog");
+  error = rcv.UnsubscribeLogs();
+  if (error)
+    printf("Failed to subscribe from logs");
+
   return 1;
 }
